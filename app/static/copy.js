66a357967f7715af943fc7b4ecfabb9cbcc62011
Donovan O'Linn <donovan.b.olinn@gmail.com>
2023-03-31 17:13:42 -0400

    adjusting the JS functions links

--- app/static/copy.js
+++ app/static/copy.js
@@ -4,33 +4,33 @@ function copytxt1() {
     let copyText = document.getElementById("basic-url")
     copyText.select();
     copyText.setSelectionRange(0,99999);
-    navigator.clipboard.writeText("https://exfor-api.herokuapp.com/api/" + copyText.value)
+    navigator.clipboard.writeText("https://https://expeditionary-force.onrender.com/api/" + copyText.value)
 }
 
 function copytxt2() {
     let copyText = document.getElementById("Species-filter-url")
     copyText.select();
     copyText.setSelectionRange(0,99999);
-    navigator.clipboard.writeText("https://exfor-api.herokuapp.com/api/Species/all/" + copyText.value)
+    navigator.clipboard.writeText("https://https://expeditionary-force.onrender.com/api/Species/all/" + copyText.value)
 }
 
 function copytxt3() {
     let copyText = document.getElementById("Character-filter-url")
     copyText.select();
     copyText.setSelectionRange(0,99999);
-    navigator.clipboard.writeText("https://exfor-api.herokuapp.com/api/character/" + copyText.value)
+    navigator.clipboard.writeText("https://https://expeditionary-force.onrender.com/api/character/" + copyText.value)
 }
 
 function copytxt4() {
     let copyText = document.getElementById("Ship-filter-url")
     copyText.select();
     copyText.setSelectionRange(0,99999);
-    navigator.clipboard.writeText("https://exfor-api.herokuapp.com/api/ship/" + copyText.value)
+    navigator.clipboard.writeText("https://https://expeditionary-force.onrender.com/api/ship/" + copyText.value)
 }
 
 function copytxt5() {
     let copyText = document.getElementById("planet-filter-url")
     copyText.select();
     copyText.setSelectionRange(0,99999);
-    navigator.clipboard.writeText("https://exfor-api.herokuapp.com/api/planet/" + copyText.value)
+    navigator.clipboard.writeText("https://https://expeditionary-force.onrender.com/api/planet/" + copyText.value)
 }
